c0ec04fd935234ad2cbfacf9f9364b9924f76ea9
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-07-22 18:32:11 -0700

    created about and terms component as well as setup routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Components
 import Header from './components/header/Header';
 import HomeGuest from './components/home-guest/HomeGuest';
 import Footer from './components/footer/Footer';
+import About from './components/About/About';
+import Terms from './components/terms/Terms';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
-            <HomeGuest />
+            <Switch>
+                <Route path='/' exact>
+                    <HomeGuest />
+                </Route>
+                <Route path='/about-us' exact>
+                    <About />
+                </Route>
+                <Route path='/terms' exact>
+                    <Terms />
+                </Route>
+            </Switch>
             <Footer />
-        </>
+        </Router>
     );
 };
 
